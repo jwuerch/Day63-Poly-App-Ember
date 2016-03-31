354a1dc5851af55285d723d043b67587cc967357
Jason & Blair <wuerchjason@gmail.com>
2016-03-31 14:08:02 -0700

    make registration 2 form appear after registration 1 form is complete

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  registerOne: true,
   firebase: Ember.inject.service(),
   actions: {
     signUp() {
@@ -12,7 +13,7 @@ export default Ember.Controller.extend({
         if (error) {
           console.log(error);
         } else {
-          this.set('addNewUser', true);
+          this.set('registerOne', false);
           controller.set('email', null);
           controller.set('password', null);
         }
--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   beforeModel() {
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('welcome');
     }
   },
-  addNewUser: false,
+  registerOne: true,
+  registerTwo: false,
   actions: {
-    saveUser(params) {
+    saveUser() {
+      this.set('registerTwo', true);
+      this.set('registerOne', false);
       var newUser = this.store.createRecord('user', params);
       newUser.save();
     }
