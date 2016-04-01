3f02579c9f2ad73accf7029f2eab0948dd51dbfa
Jason & Blair <wuerchjason@gmail.com>
2016-03-31 17:21:08 -0700

    retreive information from database

--- app/components/image-new.js
+++ app/components/image-new.js
@@ -7,7 +7,8 @@ export default Ember.Component.extend({
         title: this.get('title') ? this.get('title') : "",
         caption: this.get('caption') ? this.get('caption') : "",
         url: this.get('url') ? this.get('url') : "",
-        user: this.get('user') ? this.get('user') : ""
+        user: this.get('user') ? this.get('user') : "",
+        main: this.get('main') ? this.get('main') : false
       };
       this.sendAction('saveImage', params);
     }
--- app/routes/user.js
+++ app/routes/user.js
@@ -12,6 +12,14 @@ export default Ember.Route.extend({
       newImage.save().then(function() {
         return user.save();
       });
+      var ref = new Firebase("https://poly-match-me.firebaseio.com");
+      ref.on("value", function(snapshot) {
+        console.log(snapshot.val().images);
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+      console.log(user.id)
+      console.log(user.images)
     }
   }
 });
